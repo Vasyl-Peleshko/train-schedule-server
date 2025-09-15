236d4a1310bdc73081a89fa1c57a053ff28486c4
Vasyl Peleshko <vasylpeleshko13@gmail.com>
2025-09-16 02:29:53 +0300

    chore: set up cors

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: [
+      'https://trains-schedule-client-igi3.onrender.com',
+      'http://localhost:3000',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
